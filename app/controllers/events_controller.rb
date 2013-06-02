e3b2449d3c1d3508e83847d4d8ef5a103104435c
Barry Walker <barrywalker@gmail.com>
2013-06-02 07:34:47 -0400

    Take event type id into account when we return the count. Otherwise, we return events for all types.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
 		@create = Event.create(:user_id => current_user.id, 
       :thing_id => params[:thing_id], :event_type_id => params[:event_type_id] )
 		render :json => {:count => Event.count(:conditions => {:user_id => current_user.id, 
-      :thing_id => params[:thing_id]})}, :status => :created
+      :thing_id => params[:thing_id], :event_type_id => params[:event_type_id]})}, :status => :created
 	end
 
 end
